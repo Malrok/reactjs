dd9e4bacd4cc2a3c4392526fbb11b34c7f87cc1a
Simon Bernardin <themalrok@gmail.com>
2020-03-17 17:14:23 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import firebase from 'firebase';
-
-const config = {
-  apiKey: "AIzaSyCJhO9SPTit2418hkttbpn_KFxL-G3yyPM",
-  authDomain: "cross-platform-test.firebaseapp.com",
-  databaseURL: "https://cross-platform-test.firebaseio.com",
-  projectId: "cross-platform-test",
-  storageBucket: "cross-platform-test.appspot.com",
-  messagingSenderId: "734028203665",
-  appId: "1:734028203665:web:b11deb7aaf2b0998c52130"
-};
  
 firebase.initializeApp(config);
  
